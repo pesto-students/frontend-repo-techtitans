3cb728c720b1f049d18084d8da7c41b7e321c80e
sreekarteegala <teegalasreekar@gmail.comn>
2024-06-26 23:12:41 +0530

    Fixed image sizes and added responsive grids

--- doc_checker/src/Pages/LandingPage.jsx
+++ doc_checker/src/Pages/LandingPage.jsx
@@ -1,115 +1,115 @@
-import React, { useState, useRef } from 'react';
-import { AppBar, Toolbar, Typography, Button, Container, Box, Grid, CssBaseline, Drawer, IconButton, List, ListItem, ListItemText } from '@mui/material';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import MenuIcon from '@mui/icons-material/Menu';
-import logo from '../images/logo.png';
-import homeImage from '../images/landing_page_img.png';
-import featuresImage from '../images/features.svg';
-import aboutImage from '../images/aboutus.png';
+import React, { useState, useRef } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Grid,
+  CssBaseline,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  useMediaQuery,
+} from "@mui/material";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import logo from "../images/logo.png";
+import homeImage from "../images/landing_page_img.png";
+import featuresImage from "../images/features.svg";
+import aboutImage from "../images/aboutus.png";
 import { useNavigate } from "react-router-dom";
+import TypingEffect from "../hooks/typingEffect";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#EFF0FB',
+      main: "#EFF0FB",
     },
   },
   typography: {
-    fontFamily: 'Arial, sans-serif',
+    fontFamily: "Arial, sans-serif",
   },
   spacing: 8,
 });
 
-const Root = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  minHeight: '100vh',
+const Root = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  minHeight: "100vh",
 });
 
 const CustomAppBar = styled(AppBar)({
-  backgroundColor: '#EFF0FB',
-  boxShadow: 'none',
+  backgroundColor: "#EFF0FB",
+  boxShadow: "none",
   marginBottom: theme.spacing(4),
 });
 
-const Logo = styled('img')({
-  height: '40px',
+const Logo = styled("img")({
+  height: "40px",
   marginRight: theme.spacing(2),
 });
 
 const LogoText = styled(Typography)({
   flexGrow: 1,
-  fontSize: '32px !important',
-  fontWeight: 'bold !important',
+  fontSize: "32px !important",
+  fontWeight: "bold !important",
 });
 
-const HomeContainer = styled(Container)({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  textAlign: 'left',
-  padding: theme.spacing(4),
-  minHeight: '100vh',
-  margin: 0,
-  width: '100%',
-  maxWidth: '100%',
+const HomeContainer = styled(Grid)({
+  // display: "flex",
+  // alignItems: "center",
+  // justifyContent: "center",
+  // textAlign: "left",
+  // padding: theme.spacing(4),
+  // minHeight: "100vh",
+  // margin: 0,
+  // width: "100%",
+  // maxWidth: "100%",
 });
 
 const HomeText = styled(Box)({
-  padding: theme.spacing(4),
-});
-
-const HomeImage = styled('img')({
-  width: '100%',
-  height: '100%',
-  maxHeight: '500px',
-  objectFit: 'cover',
+  padding: theme.spacing(0, 4, 4, 4),
+  textAlign:"center"
 });
 
 const FeaturesSection = styled(Box)({
   padding: theme.spacing(4),
-  backgroundColor: '#f5f5f5',
-  textAlign: 'center',
-  minHeight: '100vh',
+  backgroundColor: "#f5f5f5",
+  textAlign: "center",
+  minHeight: "100vh",
 });
 
 const FeatureItem = styled(Box)({
   padding: theme.spacing(2),
 });
 
-const FeaturesImage = styled('img')({
-  height: 'auto',
-  maxHeight: '300px',
-  objectFit: 'cover',
+const FeaturesImage = styled("img")({
+  height: "auto",
+  maxHeight: "300px",
+  objectFit: "cover",
   marginBottom: theme.spacing(2),
 });
 
-const AboutSection = styled(Container)({
-  padding: theme.spacing(4),
-  backgroundColor: '#fff',
-  textAlign: 'center',
-  minHeight: '100vh',
+const AboutSection = styled(Grid)({
+  backgroundColor: "#fff",
 });
 
-const AboutImage = styled('img')({
-  width: '100%',
-  height: 'auto',
-  maxHeight: '300px',
-  objectFit: 'cover',
-  marginBottom: theme.spacing(2),
-});
 
 const Buttons = styled(Box)({
   marginTop: theme.spacing(2),
 });
 
 const Tabs = styled(Button)({
-  fontWeight: 'bold !important',
-  fontSize: '16px !important',
+  fontWeight: "bold !important",
+  fontSize: "16px !important",
   marginLeft: theme.spacing(2),
 });
 
 function LandingPage() {
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
   const [drawerOpen, setDrawerOpen] = useState(false);
   const navigate = useNavigate();
   const homeRef = useRef();
@@ -120,7 +120,7 @@ function LandingPage() {
     if (ref && ref.current) {
       window.scrollTo({
         top: ref.current.offsetTop - 80,
-        behavior: 'smooth',
+        behavior: "smooth",
       });
     }
   };
@@ -141,10 +141,8 @@ function LandingPage() {
         <CustomAppBar position="fixed">
           <Toolbar>
             <Logo src={logo} alt="DocChecker Logo" />
-            <LogoText variant="h6">
-              DocChecker
-            </LogoText>
-            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <LogoText variant="h6">DocChecker</LogoText>
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
               <Tabs color="inherit" onClick={() => scrollToRef(homeRef)}>
                 Home
               </Tabs>
@@ -155,7 +153,7 @@ function LandingPage() {
                 About us
               </Tabs>
             </Box>
-            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
               <IconButton
                 color="inherit"
                 aria-label="open drawer"
@@ -171,7 +169,7 @@ function LandingPage() {
           anchor="right"
           open={drawerOpen}
           onClose={handleDrawerToggle}
-          sx={{ display: { xs: 'block', md: 'none' } }}
+          sx={{ display: { xs: "block", md: "none" } }}
         >
           <Box
             sx={{
@@ -185,7 +183,10 @@ function LandingPage() {
               <ListItem button onClick={() => handleNavigationClick(homeRef)}>
                 <ListItemText primary="Home" />
               </ListItem>
-              <ListItem button onClick={() => handleNavigationClick(featureRef)}>
+              <ListItem
+                button
+                onClick={() => handleNavigationClick(featureRef)}
+              >
                 <ListItemText primary="Features" />
               </ListItem>
               <ListItem button onClick={() => handleNavigationClick(aboutRef)}>
@@ -195,29 +196,68 @@ function LandingPage() {
           </Box>
         </Drawer>
         <HomeContainer ref={homeRef}>
-          <Grid container spacing={4} alignItems="center" sx={{ width: '100%', margin: 0 }}>
-            <Grid item xs={12} md={6} sx={{ padding: 0 }}>
-              <HomeImage src={homeImage} alt="home" />
-            </Grid>
-            <Grid item xs={12} md={6}>
+          <Grid container spacing={2}>
+            {!isSmallScreen && (
+              <Grid item xs={6} className="parentDivFullHeight">
+                <img src={aboutImage} alt="landing_image" />
+              </Grid>
+            )}
+
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="start"
+              item
+              xs={12}
+              md={6}
+              mt={25}
+            >
               <HomeText>
-                <Typography variant="body1" textAlign="center" gutterBottom> 
-                  Your go-to platform for comprehensive document reviews.
-                  Whether you're a student crafting the perfect college application essay, a professional fine-tuning your resume, or a product manager refining your requirements document, we've got you covered.
+                <img src={logo} alt="DocChecker_Logo" />
+                <Typography variant="h2" mb={2} sx={{ textAlign: "center" }}>
+                  Doc Checker
+                  <TypingEffect
+                    texts={[
+                      "WHERE YOUR DOCUMENTS ARE NOT JUST REVIEWED BUT TRANSFORMED...!!!",
+                    ]}
+                    speed={100}
+                    pause={500}
+                  />
                 </Typography>
-                <Buttons textAlign="center">
-                  <Button 
-                    variant="contained" 
-                    aria-label="Login" 
-                    style={{ marginRight: 8, background: '#6FA5ED', color: '#fff' }}
+                <Typography
+                  variant="body1"
+                  textAlign="left"
+                  gutterBottom
+                  sx={{ typography: "body1", textAlign: "justify" }}
+                >
+                  Your go-to platform for comprehensive document reviews.
+                  Whether you're a student crafting the perfect college
+                  application essay, a professional fine-tuning your resume, or
+                  a product manager refining your requirements document, we've
+                  got you covered.
+                </Typography>
+                <Buttons textAlign="center" mt={3}>
+                  <Button
+                    variant="contained"
+                    aria-label="Login"
+                    style={{
+                      marginRight: 8,
+                      color: "#fff",
+                      backgroundColor: "#1976d2",
+                    }}
                     onClick={() => navigate("/login")}
                   >
                     Login
                   </Button>
-                  <Button 
-                    variant="contained" 
-                    aria-label="Sign Up" 
-                    style={{ marginRight: 8, background: '#6FA5ED', color: '#fff' }}
+                  <Button
+                    variant="contained"
+                    aria-label="Sign Up"
+                    style={{
+                      marginRight: 8,
+                      color: "#fff",
+                      backgroundColor: "#1976d2",
+                    }}
                     onClick={() => navigate("/signup-as")}
                   >
                     Sign Up
@@ -228,7 +268,7 @@ function LandingPage() {
           </Grid>
         </HomeContainer>
         <FeaturesSection ref={featureRef}>
-          <Typography variant="h2" gutterBottom style={{ fontWeight: 'bold' }}>
+          <Typography variant="h2" gutterBottom style={{ fontWeight: "bold" }}>
             Features
           </Typography>
           <Grid container spacing={3}>
@@ -236,7 +276,9 @@ function LandingPage() {
               <FeatureItem>
                 <Typography variant="h6">Expert Reviews</Typography>
                 <Typography variant="body1">
-                  Expert Reviews: Receive detailed feedback from experienced reviewers who specialize in various document types, ensuring that your work meets the highest standards.
+                  Expert Reviews: Receive detailed feedback from experienced
+                  reviewers who specialize in various document types, ensuring
+                  that your work meets the highest standards.
                 </Typography>
               </FeatureItem>
             </Grid>
@@ -247,7 +289,9 @@ function LandingPage() {
               <FeatureItem>
                 <Typography variant="h6">Secure Platform</Typography>
                 <Typography variant="body1">
-                  Secure Platform: Rest assured that your documents are safe and secure on our platform, with strict privacy measures in place to protect your sensitive information.
+                  Secure Platform: Rest assured that your documents are safe and
+                  secure on our platform, with strict privacy measures in place
+                  to protect your sensitive information.
                 </Typography>
               </FeatureItem>
             </Grid>
@@ -255,7 +299,9 @@ function LandingPage() {
               <FeatureItem>
                 <Typography variant="h6">Customized Recommendations</Typography>
                 <Typography variant="body1">
-                  Customized Recommendations: Get personalized suggestions for improving your documents, tailored to your unique goals and requirements.
+                  Customized Recommendations: Get personalized suggestions for
+                  improving your documents, tailored to your unique goals and
+                  requirements.
                 </Typography>
               </FeatureItem>
             </Grid>
@@ -263,7 +309,9 @@ function LandingPage() {
               <FeatureItem>
                 <Typography variant="h6">Fast Turnaround</Typography>
                 <Typography variant="body1">
-                  Fast Turnaround: Enjoy quick turnaround times on your document reviews, so you can make necessary revisions and submit your applications or documents with confidence.
+                  Fast Turnaround: Enjoy quick turnaround times on your document
+                  reviews, so you can make necessary revisions and submit your
+                  applications or documents with confidence.
                 </Typography>
               </FeatureItem>
             </Grid>
@@ -271,27 +319,63 @@ function LandingPage() {
               <FeatureItem>
                 <Typography variant="h6">User-Friendly Interface</Typography>
                 <Typography variant="body1">
-                  User-Friendly Interface: Navigate our intuitive platform with ease, making it simple to submit documents and track reviews.
+                  User-Friendly Interface: Navigate our intuitive platform with
+                  ease, making it simple to submit documents and track reviews.
                 </Typography>
               </FeatureItem>
             </Grid>
           </Grid>
         </FeaturesSection>
-        <AboutSection ref={aboutRef}>
-          <Typography variant="h2" gutterBottom style={{ fontWeight: 'bold' }}>
-            About us
-          </Typography>
-          <Grid container spacing={4} alignItems="center">
-            <Grid item xs={12} md={6}>
+        <AboutSection ref={aboutRef} mt={3}>
+          <Grid container spacing={2}>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              xs={12}
+            >
+              <Typography
+                variant="h2"
+                gutterBottom
+                style={{ fontWeight: "bold" }}
+              >
+                About us
+              </Typography>
+            </Grid>
+
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              item
+              xs={12}
+              md={6}
+            >
               <HomeText>
-                <Typography variant="body1" gutterBottom>
-                  At DocChecker, we're passionate about helping individuals achieve their goals through impactful document reviews. Our team of seasoned professionals brings years of experience in various industries, including education, business, and technology. We understand the importance of a well-crafted document in making a lasting impression, and we're dedicated to providing you with the tools and support you need to succeed. Trust us to elevate your documents to the next level.
+                <Typography
+                  variant="body1"
+                  textAlign="left"
+                  gutterBottom
+                  sx={{ typography: "body1", textAlign: "justify" }}
+                >
+                  At DocChecker, we're passionate about helping individuals
+                  achieve their goals through impactful document reviews. Our
+                  team of seasoned professionals brings years of experience in
+                  various industries, including education, business, and
+                  technology. We understand the importance of a well-crafted
+                  document in making a lasting impression, and we're dedicated
+                  to providing you with the tools and support you need to
+                  succeed. Trust us to elevate your documents to the next level.
                 </Typography>
               </HomeText>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <AboutImage src={aboutImage} alt="About us" />
-            </Grid>
+
+            
+              <Grid item xs={12} md={6} className="parentDivFullHeight">
+                <img src={homeImage} alt="About us" />
+              </Grid>
           </Grid>
         </AboutSection>
       </Root>
